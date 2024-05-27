3fb37140b5b5eaa6c133cfd8e460ed5f0c0face6
SAIMALK <aleesasaim496@gmail.com>
2024-05-27 11:29:37 +0500

    lazyload discarded

--- frontend/src/components/Stories.jsx
+++ frontend/src/components/Stories.jsx
@@ -1,34 +1,36 @@
-// import { Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import LazyLoad from "react-lazyload";
 
-const Stories = ({ story }) => {
+const Stories = ({ story, isLoading }) => {
   return (
-    
-      
-    <li className="zoomed-landing-page">
-    <LazyLoad>
-    <Link to={`/story/${story._id}`}>
-      <img
-        src={story.cover}
-        alt="loading cover..."
-        loading="lazy"
-      
-        style={{ width: "190px" }}
-      />
-      </Link>
-    </LazyLoad>
-    <Link to={`/story/${story._id}`}>
-  <div className="overlay">
-    <span style={{ color: "white !important" }}>{story.title}</span>
-  </div>
-</Link>
+    isLoading ? (
+      <div
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          minHeight: "80vh",
+        }}
+      >
+        <span className="loader"></span>
+      </div>
+    ) : (
+      <li className="zoomed-landing-page">
+        <Link to={`/story/${story._id}`}>
+          <img
+            src={story.cover}
+            alt="loading cover..."
+            loading="lazy"
+            style={{ width: "190px" }}
+          />
+        </Link>
+        <Link to={`/story/${story._id}`}>
+          <div className="overlay">
+            <span style={{ color: "white" }}>{story.title}</span>
+          </div>
+        </Link>
+      </li>
+    )
+  );
+};
 
-  </li>
-   
-)}
-
-
-
-    export default Stories;
-    
+export default Stories;
